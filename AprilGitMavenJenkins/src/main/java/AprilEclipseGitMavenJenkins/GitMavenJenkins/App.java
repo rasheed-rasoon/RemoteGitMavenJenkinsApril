f5278e3155b8a3259e2d0191989897bba5b980c6
rasheed-rasoon <rasheed.rasoon@gmail.com>
2019-04-29 16:57:50 +0530

    First commit in April

---
+++ AprilGitMavenJenkins/src/main/java/AprilEclipseGitMavenJenkins/GitMavenJenkins/App.java
@@ -0,0 +1,13 @@
+package AprilEclipseGitMavenJenkins.GitMavenJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
