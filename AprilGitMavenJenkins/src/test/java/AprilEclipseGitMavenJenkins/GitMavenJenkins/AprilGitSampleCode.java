b6688e81399abf755fc11043ad595f5657a5b049
rasheed-rasoon <rasheed.rasoon@gmail.com>
2019-04-29 17:06:13 +0530

    second commit

---
+++ AprilGitMavenJenkins/src/test/java/AprilEclipseGitMavenJenkins/GitMavenJenkins/AprilGitSampleCode.java
@@ -0,0 +1,11 @@
+package AprilEclipseGitMavenJenkins.GitMavenJenkins;
+
+import org.testng.annotations.Test;
+
+public class AprilGitSampleCode {
+  @Test
+  public void sample() {
+	  System.out.println("April Git Sample working fine");
+	  System.out.println("2nd commit");
+  }
+}
