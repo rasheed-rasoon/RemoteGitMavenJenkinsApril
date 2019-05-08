e5f459925108ab8ac3ee639279735457d84112bf
rasheed-rasoon <rasheed.rasoon@gmail.com>
2019-05-08 08:07:53 +0530

    regression 3 added in regression package

---
+++ AprilGitMavenJenkins/src/test/java/regressionGroup/regressionTestCases3Test.java
@@ -0,0 +1,10 @@
+package regressionGroup;
+
+import org.testng.annotations.Test;
+
+public class regressionTestCases3Test {
+  @Test
+  public void f() {
+	  System.out.println("Regression testcases 3s");
+  }
+}
